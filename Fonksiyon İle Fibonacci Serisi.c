3f2c603fc6d0d2e469a45d8d7fa322a8c06e394e
beyza767 <149117611+beyza767@users.noreply.github.com>
2023-12-06 17:39:19 +0300

    Update Fonksiyon İle Fibonacci Serisi.c

--- Fonksiyon İle Fibonacci Serisi.c
+++ Fonksiyon İle Fibonacci Serisi.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void fibonacciSerisi(int sayi)
-{
-    int ilkSayi=0;
-    int ikinciSayi=1;
-    int toplam=0;
-
-    printf("%d\t",ilkSayi);
-    printf("%d\t",ikinciSayi);
-
-    for(int i=0; i<sayi-2; i++)
-    {
-        toplam=ilkSayi+ikinciSayi;
-        ilkSayi=ikinciSayi;
-        ikinciSayi=toplam;
-        printf("%d\t",toplam);
-
-    }
-    printf("\n");
-
-}
-
-
-
 int main()
 {
-    int sayi;
-    printf("Kac Adet Fibonacci Sayisi Uretilecek : ");
-    scanf("%d",&sayi);
-    printf("\n");
-    fibonacciSerisi(sayi);
 
-    return 0;
+int n, first = 0, second = 1, next, c;
+
+   printf("Enter the number of terms\n");
+   scanf("%d",&n);
+
+   printf("First %d terms of Fibonacci series are :-\n",n);
+
+   for ( c = 0 ; c < n ; c++ )
+   {
+      if ( c <= 1 )
+         next = c;
+      else
+      {
+         next = first + second;
+         first = second;
+         second = next;
+      }
+      printf("%d\n",next);
+   }
 }
